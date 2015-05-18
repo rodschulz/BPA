90169ffecf3d779e4432d4824086297c12a61602
rodrigo <rodrigo@gato>
2015-05-18 12:45:55 -0300

    Added ignore file, cmakelist file and generation utility scripts

---
+++ make.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd ./build
+make -j4
+cd ..
---
+++ regenerate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ ! -d "./build" ]; then
+  mkdir ./build/
+fi
+
+rm -rf ./build/*
+cd ./build
+cmake -DCMAKE_BUILD_TYPE=Debug ../src/
+cd ..
