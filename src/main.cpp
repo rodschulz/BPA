c51fa7a249ea2156c91dfdfaebd6005cee945908
rodrigo <rodrigo@gato>
2015-05-18 13:14:41 -0300

    Added initial main file

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <pcl/common/common.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/surface/mls.h>
+#include <pcl/surface/poisson.h>
+#include <pcl/io/vtk_io.h>
+
+using namespace pcl;
+
+int main(int argc, char **argv)
+{
+	return EXIT_SUCCESS;
+}
